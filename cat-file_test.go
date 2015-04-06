fe89ee30bbcdfdf376beae530cc53f967012f31c
aditya <dev@chimeracoder.net>
2015-04-06 16:05:37 -0400

    Update tests to include parsing a non-initial commit as well

--- cat-file_test.go
+++ cat-file_test.go
@@ -25,11 +25,11 @@ First commit. Create .gitignore`
 	}
 }
 
-func Test_parseObj(t *testing.T) {
+func Test_parseObjInitialCommit(t *testing.T) {
 	expected := GitObject{
-		Type: "commit",
-		Tree: "9de6c72106b169990a83ce7090c7cad84b6b506b",
-		//Parents:   nil,
+		Type:      "commit",
+		Tree:      "9de6c72106b169990a83ce7090c7cad84b6b506b",
+		Parents:   nil,
 		Author:    "aditya <dev@chimeracoder.net> 1428075900 -0400",
 		Committer: "aditya <dev@chimeracoder.net> 1428075900 -0400",
 		Message:   "First commit. Create .gitignore",
@@ -41,8 +41,36 @@ author aditya <dev@chimeracoder.net> 1428075900 -0400
 committer aditya <dev@chimeracoder.net> 1428075900 -0400
 
 First commit. Create .gitignore`
-
 	result, err := parseObj(input)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Expected and result don't match:\n%+v\n%+v", expected, result)
+	}
+}
+
+func Test_parseObj(t *testing.T) {
+	const inputSha = "3ead3116d0378089f5ce61086354aac43e736b01"
+
+	expected := GitObject{
+		Type:      "commit",
+		Tree:      "d22fc8a57073fdecae2001d00aff921440d3aabd",
+		Parents:   []string{"1d833eb5b6c5369c0cb7a4a3e20ded237490145f"},
+		Author:    "aditya <dev@chimeracoder.net> 1428349896 -0400",
+		Committer: "aditya <dev@chimeracoder.net> 1428349896 -0400",
+		Message:   "Remove extraneous logging statements\n",
+		Size:      "243",
+	}
+
+	str, err := CatFile(inputSha)
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err := parseObj(str)
 	if err != nil {
 		t.Error(err)
 		return
