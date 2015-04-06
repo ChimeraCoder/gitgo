1d833eb5b6c5369c0cb7a4a3e20ded237490145f
aditya <dev@chimeracoder.net>
2015-04-06 15:49:15 -0400

    Parse git object into struct and add corresponding test

--- cat-file.go
+++ cat-file.go
@@ -2,10 +2,32 @@ package gitgo
 
 import (
 	"compress/zlib"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 )
+
+type KeyType string
+
+const (
+	TreeKey      KeyType = "tree"
+	ParentKey            = "parent"
+	AuthorKey            = "author"
+	CommitterKey         = "committer"
+)
+
+type GitObject struct {
+	Type string
+
+	Tree      string
+	Parents   []string
+	Author    string
+	Committer string
+	Message   string
+	Size      string
+}
 
 func CatFile(inputSha string) (result string, err error) {
 
@@ -28,4 +50,40 @@ func CatFile(inputSha string) (result string, err error) {
 	}
 
 	return string(bts), nil
+}
+
+func parseObj(obj string) (result GitObject, err error) {
+
+	parts := strings.Split(obj, "\x00")
+	parts = strings.Fields(parts[0])
+	result.Type = parts[0]
+	result.Size = parts[1]
+	nullIndex := strings.Index(obj, "\x00")
+
+	obj = obj[nullIndex+1:]
+	lines := strings.Split(obj, "\n")
+
+	for i, line := range lines {
+		// The next line is the commit message
+		if len(strings.Fields(line)) == 0 {
+			result.Message = strings.Join(lines[i+1:], "\n")
+			break
+		}
+		parts := strings.Fields(line)
+		key := parts[0]
+		switch KeyType(key) {
+		case TreeKey:
+			result.Tree = parts[1]
+		case ParentKey:
+			result.Parents = append(result.Parents, parts[1])
+		case AuthorKey:
+			result.Author = strings.Join(parts[1:], " ")
+		case CommitterKey:
+			result.Committer = strings.Join(parts[1:], " ")
+		default:
+			err = fmt.Errorf("Encounterd unknown field in commit: %s", key)
+			return
+		}
+	}
+	return
 }
--- cat-file_test.go
+++ cat-file_test.go
@@ -1,6 +1,8 @@
 package gitgo
 
 import (
+	"log"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -22,4 +24,42 @@ First commit. Create .gitignore`
 
 		t.Errorf("Expected and result don't match:\n%s \n\n\nresult: \n%s", expected, result)
 	}
+}
+
+func Test_parseObj(t *testing.T) {
+	expected := GitObject{
+		Type: "commit",
+		Tree: "9de6c72106b169990a83ce7090c7cad84b6b506b",
+		//Parents:   nil,
+		Author:    "aditya <dev@chimeracoder.net> 1428075900 -0400",
+		Committer: "aditya <dev@chimeracoder.net> 1428075900 -0400",
+		Message:   "First commit. Create .gitignore",
+		Size:      "190",
+	}
+
+	const input = "commit 190\x00" + `tree 9de6c72106b169990a83ce7090c7cad84b6b506b
+author aditya <dev@chimeracoder.net> 1428075900 -0400
+committer aditya <dev@chimeracoder.net> 1428075900 -0400
+
+First commit. Create .gitignore`
+
+	result, err := parseObj(input)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Expected and result don't match:\n%+v\n%+v", expected, result)
+
+		numFields := reflect.ValueOf(expected).NumField()
+		for i := 0; i < numFields; i++ {
+			if !reflect.DeepEqual(reflect.ValueOf(expected).Field(i), reflect.ValueOf(result).Field(i)) {
+				log.Printf("Different value for %s", reflect.TypeOf(expected).Field(i).Name)
+				log.Printf("Different value for %s", reflect.TypeOf(expected).Field(i))
+			}
+		}
+
+	}
+	log.Print(reflect.DeepEqual(expected.Parents, result.Parents))
 }
