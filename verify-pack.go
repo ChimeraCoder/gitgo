28f9a5dab2726e11a61f35d313911bfb66d7154f
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-04-13 15:17:48 -0400

    Writing function to get idx file path

--- verify-pack.go
+++ verify-pack.go
@@ -8,35 +8,40 @@ import (
 	"path"
 )
 
-type KeyType string
-type SHA string
-
-const (
-	TreeKey      KeyType = "tree"
-	ParentKey            = "parent"
-	AuthorKey            = "author"
-	CommitterKey         = "committer"
-)
-
-func CatFile(input SHA) (result string, err error) {
-
-	filename := path.Join(".git", "objects", string(input[:2]), string(input[2:]))
-
-	f, err := os.Open(filename)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
-	r, err := zlib.NewReader(f)
-	if err != nil {
-		return
-	}
-
-	bts, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	return string(bts), nil
+func getIdxPath(dotGitRootPath string) (idxFilePath string, err error) {
+	files, _ := path.filepath.Glob(path.Join(idxFilePath, "*.idx"))
+	return files[0]
 }
+
+// type KeyType string
+// type SHA string
+//
+// const (
+// 	TreeKey      KeyType = "tree"
+// 	ParentKey            = "parent"
+// 	AuthorKey            = "author"
+// 	CommitterKey         = "committer"
+// )
+
+// func CatFile(input SHA) (result string, err error) {
+//
+// 	filename := path.Join(".git", "objects", string(input[:2]), string(input[2:]))
+//
+// 	f, err := os.Open(filename)
+// 	if err != nil {
+// 		return
+// 	}
+// 	defer f.Close()
+//
+// 	r, err := zlib.NewReader(f)
+// 	if err != nil {
+// 		return
+// 	}
+//
+// 	bts, err := ioutil.ReadAll(r)
+// 	if err != nil {
+// 		return
+// 	}
+//
+// 	return string(bts), nil
+// }
