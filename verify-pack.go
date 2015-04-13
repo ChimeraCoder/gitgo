a2127b0b161b067c72d7f43d2349f9a335ea6f84
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-04-13 14:56:22 -0400

    adds copy of current "dot" git dir into test data dir

---
+++ verify-pack.go
@@ -0,0 +1,42 @@
+package gitgo
+
+import (
+	"compress/zlib"
+
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+type KeyType string
+type SHA string
+
+const (
+	TreeKey      KeyType = "tree"
+	ParentKey            = "parent"
+	AuthorKey            = "author"
+	CommitterKey         = "committer"
+)
+
+func CatFile(input SHA) (result string, err error) {
+
+	filename := path.Join(".git", "objects", string(input[:2]), string(input[2:]))
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	r, err := zlib.NewReader(f)
+	if err != nil {
+		return
+	}
+
+	bts, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+
+	return string(bts), nil
+}
