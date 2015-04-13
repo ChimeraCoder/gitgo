273d641de768af9465fc6b77483f7371a5c94803
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-04-13 16:52:16 -0400

    amidst reading pack idx files

--- verify-pack.go
+++ verify-pack.go
@@ -1,7 +1,11 @@
-package gitgo
+// package gitgo
+package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path"
 	"path/filepath"
 )
@@ -12,46 +16,32 @@ func GetIdxPath(dotGitRootPath string) (idxFilePath string, err error) {
 	return
 }
 
-func GetIdxPath(dotGitRootPath string) (idxFilePath string, err error) {
-	files, err := filepath.Glob(path.Join(dotGitRootPath, "objects/pack", "*.idx"))
-	idxFilePath = files[0]
-	return
+func VerifyPack(idxFilePath string) (verifyPackResult string, err error) {
+	f, err := os.Open(idxFilePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	r, err := zlib.NewReader(f)
+	if err != nil {
+		return
+	}
+
+	bts, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+
+	return string(bts), nil
 }
 
-// func main() {
-// 	// fmt.Println("i got...")
-// 	fmt.Println(getIdxPath("test_data/dot_git"))
-// }
+func main() {
+	path, _ := GetIdxPath("test_data/dot_git")
+	fmt.Println("idx path is: ")
+	fmt.Println(path)
 
-// type KeyType string
-// type SHA string
-//
-// const (
-// 	TreeKey      KeyType = "tree"
-// 	ParentKey            = "parent"
-// 	AuthorKey            = "author"
-// 	CommitterKey         = "committer"
-// )
-
-// func CatFile(input SHA) (result string, err error) {
-//
-// 	filename := path.Join(".git", "objects", string(input[:2]), string(input[2:]))
-//
-// 	f, err := os.Open(filename)
-// 	if err != nil {
-// 		return
-// 	}
-// 	defer f.Close()
-//
-// 	r, err := zlib.NewReader(f)
-// 	if err != nil {
-// 		return
-// 	}
-//
-// 	bts, err := ioutil.ReadAll(r)
-// 	if err != nil {
-// 		return
-// 	}
-//
-// 	return string(bts), nil
-// }
+	fmt.Println("Content is: ")
+	idxFileContent, _ := VerifyPack(path)
+	fmt.Println(idxFileContent)
+}
